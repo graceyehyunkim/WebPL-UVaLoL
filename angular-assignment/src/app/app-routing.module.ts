64d2fa2f37ce5f2195918d57ba4514f4bba88478
lki9419 <kl8jc@virginia.edu>
2020-03-04 18:27:50 -0500

    Angular navbar

--- angular-assignment/src/app/app-routing.module.ts
+++ angular-assignment/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DraftComponent } from './draft/draft.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'draft', component: DraftComponent },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-assignment/src/app/app.module.ts
+++ angular-assignment/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DraftComponent } from './draft/draft.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DraftComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
