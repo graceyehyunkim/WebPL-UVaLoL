4fd53dd1861938a0d5227e93e972771fe88d5a1a
lki9419 <kl8jc@virginia.edu>
2020-03-04 22:05:48 -0500

    Navbar and Signup page complete

--- angular-assignment/src/custom.js
+++ angular-assignment/src/custom.js
@@ -1,9 +1,10 @@
+//Alerts user when login information is invalid
 function validateInfo() {
     if (document.getElementById("loginID").value === ''){
-        alert('Please enter valid ID');
+        alert('Please enter valid ID!');
     }
     else if (document.getElementById("loginPassword").value === ''){
-        alert('Please enter valid Password');
+        alert('Please enter valid Password!');
     }
     else {
         alert('Logged In');
