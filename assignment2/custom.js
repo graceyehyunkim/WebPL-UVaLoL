91dcd9c7064d72b8a6f1b9dba127b99928b90db2
Grace <yk8mp@virginia.edu>
2020-03-05 12:48:55 -0500

    More javascript
    Should be finished (see readme)

--- assignment2/custom.js
+++ assignment2/custom.js
@@ -1,16 +1,24 @@
 //Alerts user when login information is invalid
 const validateInfo = () => {
-    if (document.getElementById("loginID").value === '') {
-        alert('Please enter valid ID!');
-    } else if (document.getElementById("loginPassword").value === '') {
-        alert('Please enter valid Password!');
-    } else {
-        alert('Logged In');
+        if (document.getElementById("loginID").value === '') {
+            alert('Please enter valid ID!');
+        } else if (document.getElementById("loginPassword").value === '') {
+            alert('Please enter valid Password!');
+        } else {
+            alert('Logged In');
+        }
     }
-}
-//confirmation for leaving the page for discord link
+    //confirmation for leaving the page for discord link
 document.getElementById("discord").addEventListener("click", function() {
-    if(confirm('You are about to leave the page. Continue?') == true) {
+    if (confirm('You are about to leave the page. Continue?') == true) {
         window.location.href = "https://discord.gg/erFKKhc"
     }
 });
+
+document.getElementById("rulebook").onmouseover = function() {
+    document.getElementById("rulebook").innerHTML = "Sorry! Rulebook under reconstruction";
+}
+
+document.getElementById("rulebook").onmouseout = function() {
+    document.getElementById("rulebook").innerHTML = "Link to the 2019-2020 rulebook";
+}
